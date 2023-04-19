3fbc970c6188d9aba2f63d63ed3c557d9577a131
Aduiduidui <thanakrit.earth@gmail.com>
2023-04-19 10:41:59 +0700

    fix after review

--- lib/modules/search/search_view.dart
+++ lib/modules/search/search_view.dart
@@ -36,120 +36,135 @@ class _SearchViewState extends State<SearchView> {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(188, 244, 222, 1),
       body: SafeArea(
-        child: FutureBuilder(
-          future: loadListOfProduct(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Row(
-                      children: [
-                        //** TextField **//
-                        Expanded(
-                          flex: 3,
-                          child: SizedBox(
-                            height: 35.0,
-                            child: TextField(
-                              onChanged: (val) {
-                                setState(() {
-                                  searchText = val;
-                                });
-                              },
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Search Here',
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 3.5,
-                        ),
-                        //** ID Button **//
-                        Expanded(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 238, 100, 143),
-                                fixedSize: const Size(5.0, 10.0)),
-                            onPressed: () {
-                              setState(() {
-                                //TODOshow speed
-                                if (searchText == '') {
-                                  showProducts = products;
-                                } else {
-                                  final stopwatch = Stopwatch();
-                                  stopwatch.start();
-                                  showProducts = getById(searchText);
-                                  stopwatch.stop();
-                                  print(
-                                      '${stopwatch.elapsedMicroseconds * 0.001} ms');
-                                }
-                              });
-                            },
-                            child: const Text(
-                              'ID',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                        //** Name Button **//
-                        const SizedBox(
-                          width: 3.5,
-                        ),
-                        Expanded(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 230, 129, 146),
-                                fixedSize: const Size(5.0, 10.0)),
-                            onPressed: () {
-                              setState(() {
-                                if (searchText == '') {
-                                  showProducts = products;
-                                } else {
-                                  showProducts =
-                                      getByName(searchText, products);
-                                }
-                              });
-                            },
-                            child: const Text(
-                              'Name',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+        child: listOfProducts(),
+      ),
+    );
+  }
+
+  FutureBuilder<List<Product>> listOfProducts() {
+    return FutureBuilder(
+      future: loadListOfProduct(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: Row(
+                  children: [
+                    textField(),
+                    const SizedBox(
+                      width: 3.5,
                     ),
-                  ),
-                  //** List Productshow **//
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: showProducts.length,
-                      itemBuilder: (contex, index) {
-                        return CardProduct(
-                          showProducts: showProducts,
-                          index: index,
-                        );
-                      },
+                    idButton(),
+                    const SizedBox(
+                      width: 3.5,
                     ),
-                  ),
-                ],
-              );
+                    nameButton(),
+                  ],
+                ),
+              ),
+              listShowProducts(),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Expanded listShowProducts() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: showProducts.length,
+        itemBuilder: (contex, index) {
+          return CardProduct(
+            showProducts: showProducts,
+            index: index,
+          );
+        },
+      ),
+    );
+  }
+
+  Expanded nameButton() {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 230, 129, 146),
+            fixedSize: const Size(5.0, 10.0)),
+        onPressed: () {
+          setState(() {
+            if (searchText == '') {
+              showProducts = products;
             } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+              final stopwatch = Stopwatch();
+              stopwatch.start();
+              showProducts = getByName(key: searchText, allProduct: products);
+              stopwatch.stop();
+              print('${stopwatch.elapsedMicroseconds} mico sec');
             }
+          });
+        },
+        child: const Text(
+          'Name',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded idButton() {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 238, 100, 143),
+            fixedSize: const Size(5.0, 10.0)),
+        onPressed: () {
+          setState(() {
+            if (searchText == '') {
+              showProducts = products;
+            } else {
+              final stopwatch = Stopwatch();
+              stopwatch.start();
+              showProducts = getById(searchText);
+              stopwatch.stop();
+              print('${stopwatch.elapsedMicroseconds * 0.001} ms');
+            }
+          });
+        },
+        child: const Text(
+          'ID',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Expanded textField() {
+    return Expanded(
+      flex: 3,
+      child: SizedBox(
+        height: 35.0,
+        child: TextField(
+          onChanged: (val) {
+            setState(() {
+              searchText = val;
+            });
           },
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Search Here',
+          ),
         ),
       ),
     );
--- lib/modules/search/search_view_model.dart
+++ lib/modules/search/search_view_model.dart
@@ -12,27 +12,29 @@ Future<List<Product>> loadListOfProduct() async {
 }
 
 Map<String, dynamic> mapBarcode = {};
-Map<String, dynamic> mapSku = {};
+Map<String, String> mapSku = {};
 
 void createMap(List<Product> data) {
   for (int i = 0; i < data.length; i++) {
     mapBarcode[data[i].barcode] = data[i];
-    mapSku[data[i].sku] = data[i];
+    mapSku[data[i].sku] = data[i].barcode;
   }
 }
 
 List<Product> getById(String key) {
   List<Product> tmp = [];
-  if (mapBarcode[key] != null)
+  if (mapBarcode[key] != null) {
     tmp.add(mapBarcode[key]);
-  else if (mapSku[key] != null)
-    tmp.add(mapSku[key]);
-  else
+  } else if (mapSku[key] != null) {
+    tmp.add(mapBarcode[mapSku[key]]);
+  } else {
     tmp = [];
+  }
   return tmp;
 }
 
-List<Product> getByName(String key, List<Product> allProduct) {
+List<Product> getByName(
+    {required String key, required List<Product> allProduct}) {
   List<Product> results = [];
   results = allProduct
       .where((product) =>
